adcf0ee51c216c574e46ff17e46bfe334d282996
MIZANUR RAHMAN PRANTO <62213761+mizanurrahmanpranto@users.noreply.github.com>
2021-04-01 02:33:38 +0800

    Add files via upload

---
+++ src/Subtotal.js
@@ -0,0 +1,40 @@
+import "./Subtotal.css";
+
+import CurrencyFormat from "react-currency-format";
+import React from "react";
+import { getBasketTotal } from "./reducer";
+import { useHistory } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+
+function Subtotal() {
+  const history = useHistory();
+  const [{ basket }, dispatch] = useStateValue();
+
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal ({basket.length} items): <strong>{value}</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={getBasketTotal(basket)}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+
+      <button onClick={(e) => history.push("./Payment")}>
+        Proceed to Checkout
+      </button>
+    </div>
+  );
+}
+
+export default Subtotal;
